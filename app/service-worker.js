8d5fe834516f31e845d33bec54d8e571e6d48352
Maye Edwin <mayedwine1@gmail.com>
2019-08-27 07:08:21 +0300

    delete unused

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,50 +1,26 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+if (workbox) {
+  console.log('[ PWA Fire Bundle ] Hello from Workbox');
 
-/* A project PWA Fire written. All writes reserved https://pwafire.org 2018.
-        Author : Maye Edwin https://twitter.com/mayeedwin1
-        */
+  /* change strategy method to fit your pwa needs,
+    update RegExp dir/route and cache name */
+  workbox.routing.registerRoute(
+    new RegExp('app'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: './',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 1,
+        }),
+      ],
+    })
+  );
 
-  self.addEventListener('fetch', function(event) {
-            event.respondWith(caches.open('cache').then(function(cache) {
-            return cache.match(event.request).then(function(response) {
-            console.log("cache request: " + event.request.url);
-            var fetchPromise = fetch(event.request).then(function(networkResponse) {           
-            // if we got a response from the cache, update the cache                   
-            console.log("fetch completed: " + event.request.url, networkResponse);
-            if (networkResponse) {
-                console.debug("updated cached page: " + event.request.url, networkResponse);
-                  cache.put(event.request, networkResponse.clone());}
-                    return networkResponse;
-                      }, function (event) {   
-            // rejected promise - just ignore it, we're offline!   
-                      console.log("Error in fetch()", event);
-                      event.waitUntil(
-                      caches.open('cache').then(function(cache) { 
-            // our cache is named *cache* in the caches.open() above
-                      return cache.addAll
-                      ([                   
-                    '/', // do not remove this
-                    '/index.html', //default
-                    '/index.html?homescreen=1', 
-                    '/?homescreen=1', 
-                    '/css/styles.css',
-                    '/js/page.mini.js',
-                    '/js/script.js',
-                    '/images/logo/*',
-                    '/images/people/*',
-                    '/images/covers/*',
-                    '/images/*',
-                    '/images/thumb/devfestRV.jpg',   
-                    ]);
-                    })
-                    );
-                    });
-            // respond from the cache, or the network
-              return response || fetchPromise;
-            });
-            }));
-            });
-            
-            self.addEventListener('install', function(event) {
-              self.skipWaiting();
-              console.log("Latest version installed!");
-            });
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
+} else {
+  console.log('Boo! Workbox failed to load 😬');
+}
